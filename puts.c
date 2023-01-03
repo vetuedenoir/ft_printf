cc98274862313b28f8fde7a53227f9e919353b78
vetuedenoir <kscordel@e3r9p18.clusters.42paris.fr>
2023-01-03 20:51:45 +0100

    lol

--- puts.c
+++ puts.c
@@ -6,11 +6,16 @@
 /*   By: kscordel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/27 17:40:04 by kscordel          #+#    #+#             */
-/*   Updated: 2023/01/03 17:40:27 by kscordel         ###   ########.fr       */
+/*   Updated: 2023/01/03 20:51:32 by kscordel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "printf.h"
+
+int	ft_putchar(char c)
+{
+	return (write(1, &c, 1));
+}
 
 int	ft_putstr(char *str)
 {
@@ -22,53 +27,28 @@ int	ft_putstr(char *str)
 int	ft_putnbr_base(int nbr, char *base, int b)
 {
 	long	nb;
+	int	count;
 
 	nb = nbr;
+	count = 0;
 	if (nb < 0)
 	{
-		write(1, '-', 1);
+		count += write(1, '-', 1);
 		nb = nb * -1;
 	}
 	if (nb >= b)
-		ft_putnbr_base(nb / b, base);
-	ft_putchar_fd(base[nb % b], 1);
-	return (ft_nbrlen(nbr, b));
+		count += ft_putnbr_base(nb / b, base);
+	count += ft_putchar(base[nb % b]);
+	return (count);
 }
 
 int	ft_putnbru(unsigned long nbr)
 {
+	int	count;
+
+	count = 0;
 	if (nbr > 9)
-		ft_putnbru(nbr / 10);
-	ft_putchar_fd(nbr % 10 + 48, fd);
-	return (ft_nbrulen(nbr));
-}
-
-static int	ft_nbrlen(int nbr, int b)
-{
-	int	i;
-
-	i = 0;
-	if (nbr == 0)
-		return (1);
-	while (nbr != 0)
-	{
-		nbr /= b;
-		i++;
-	}
-	return (i);
-}
-
-static int	ft_nbulen(unsigned long nbr)
-{
-	int	i;
-
-	i = 0;
-	if (nbr == 0)
-		return (1);
-	while (nbr != 0)
-	{
-		nbr /= 10;
-		i++;
-	}
-	return (i);
+		count += ft_putnbru(nbr / 10);
+	count += ft_putchar(nbr % 10 + 48);
+	return (count);
 }
